681d63504314ae196046faa92f3cad6f575a5da2
yuchen <s9973938@gmail.com>
2023-03-10 08:18:58 +0800

    feat: toRaw & markRaw

---
+++ 02_vue3/vue3_study/14_readonly & shallowReadonly/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2>readonly & shallowReadonly</h2>
+  <h3>state:{{ state }}</h3>
+ <button @click="update">更新數據</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, readonly, shallowReadonly } from 'vue';
+
+export default defineComponent ({
+  name: 'APP',
+  setup(){
+    const state = reactive({
+      name:'amy',
+      age:20,
+      car: {
+        name: 'bmw',
+        color: 'yellow',
+      }
+    });
+    // 只讀的數據---深度的只讀
+    const state2 = readonly(state);
+    // 只讀的數據---淺的只讀
+    const state3 = shallowReadonly(state)
+    const update = ()=>{
+      // state2.name += '---'
+      // state2.car.name += '---';
+
+      // state3.name += '---';
+      state3.car.name += '---';
+    }
+
+    return {
+      state,
+      state2,
+      update,
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
--- 02_vue3/vue3_study/src/App.vue
+++ 02_vue3/vue3_study/src/App.vue
@@ -1,39 +1,48 @@
 <template>
-  <h2>readonly & shallowReadonly</h2>
+  <h2>toRaw & markRaw</h2>
   <h3>state:{{ state }}</h3>
- <button @click="update">更新數據</button>
+  <button @click="toRawClick">測試toRaw</button>
+  <button @click="markRawClick">測試markRaw</button>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, readonly, shallowReadonly } from 'vue';
-
+import { defineComponent, reactive, toRaw, markRaw } from 'vue';
+interface UserInfo {
+  name: string;
+  age: number;
+  likes?: string[];
+}
 export default defineComponent ({
   name: 'APP',
   setup(){
-    const state = reactive({
-      name:'amy',
+    const state = reactive<UserInfo>({
+      name:'ppp',
       age:20,
-      car: {
-        name: 'bmw',
-        color: 'yellow',
-      }
     });
-    // 只讀的數據---深度的只讀
-    const state2 = readonly(state);
-    // 只讀的數據---淺的只讀
-    const state3 = shallowReadonly(state)
-    const update = ()=>{
-      // state2.name += '---'
-      // state2.car.name += '---';
-
-      // state3.name += '---';
-      state3.car.name += '---';
-    }
-
+    const toRawClick = ()=>{
+      console.log('toRawClick');
+      // 把代理對象變成了普通對象，數據變化，介面部變化
+      const user = toRaw(state);
+      user.name += '=='
+    };
+    const markRawClick = ()=>{
+      // state.likes = ['吃','喝'];
+      // state.likes[0] += '==';
+      // console.log('markRawClick', state.likes);
+      const likes = ['吃','喝'];
+      // markRaw標記對象數據的對象，不能再成為代理對象(不能更改)
+      state.likes = markRaw(likes);
+      setInterval(()=>{
+        if(state.likes){
+          console.log('定時器', state.likes);
+          state.likes[0] += '=';
+        }
+      },1000);
+    };
     return {
       state,
-      state2,
-      update,
+      toRawClick,
+      markRawClick,
     }
   }
 })
