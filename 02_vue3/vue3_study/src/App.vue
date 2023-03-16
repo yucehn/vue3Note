e08f02dbe4669a6d0fe1e66266bb7078cbdb02fb
yuchen <s9973938@gmail.com>
2023-03-16 21:29:20 +0800

    feat:provide和injec / CSSProperties

--- 02_vue3/vue3_study/src/App.vue
+++ 02_vue3/vue3_study/src/App.vue
@@ -1,15 +1,27 @@
+
 <template>
-  <h2></h2>
+  <h2>provide和inject</h2>
+  <p>當前顏色:{{ color }}</p>
+  <button @click="color='red'">紅色</button>
+  <button @click="color='yellow'">黃色</button>
+  <button @click="color='green'">綠色</button>
+  <hr/>
+  <Son/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent,provide,ref } from 'vue';
+import Son from './components/Son.vue'
 export default defineComponent ({
   name: 'APP',
+  components:{Son},
   setup(){
-
+    // 響應式的數據
+    const color = ref('red');
+    // 提供數據
+    provide('color', color);
     return {
+      color,
     }
   }
 })
---
+++ 02_vue3/vue3_study/src/components/GrandSon.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3 :style="{ color: activeColor.color }">GrandSon的子集組件 {{activeColor}}</h3>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject, CSSProperties,ref  } from 'vue'
+
+export default defineComponent({
+    setup() {
+        // 注入操作
+        const activeColor = ref<CSSProperties>({});
+            activeColor.value.color = inject('color');
+        return {
+            activeColor,
+        }
+    },
+})
+</script>
