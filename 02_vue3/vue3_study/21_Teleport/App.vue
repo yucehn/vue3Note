90a8c00adb3483db0d5f1053414a17491d56be8d
yuchen <s9973938@gmail.com>
2023-06-19 11:07:30 +0800

    feat: Teleport & Suspense

--- 02_vue3/vue3_study/21_Teleport/App.vue
+++ 02_vue3/vue3_study/21_Teleport/App.vue
@@ -1,13 +1,18 @@
+
 <template>
-  <div>
-  </div>
+  <h2>Teleport</h2>
+ 
+  <hr/>
+  <ModalButton />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ModalButton from './ModalButton.vue';
 
 export default defineComponent ({
   name: 'APP',
+  components:{ModalButton},
   setup(){ }
 })
 </script>
---
+++ 02_vue3/vue3_study/22_Suspense/AsyncComponent.vue
@@ -0,0 +1,20 @@
+<template>
+	<h3>AsyncComponent子級組建</h3>
+	msg: {{ msg }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	setup() {
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				resolve({
+					msg:'resolve'
+				})
+			},2000)
+		})
+	},
+})
+</script>
